adeade639f644827970d727abfbe852b22d649ad
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2023-05-28 22:50:20 -0300

    Add files via upload

--- assets/JavaScript/program.js
+++ assets/JavaScript/program.js
@@ -1,3 +1,4 @@
+/* Popap */
 var toast = document.getElementById('_toast')
 
 var btn_close = document.getElementById('btn-close')
@@ -8,11 +9,51 @@ btn_nao = addEventListener('click', ClicarNao)
 
 function ClicarClose(){
     toast.style.display = 'none'
-};
+}
 
 
 
-var links = document.getElementsByTagName('a');
-    for (var i=0, len=links.length; i < len; i ++) {
-        links[i].target = '_blank';
-    }
+
+
+
+
+/* Imgs contato */
+/* Imgs contato / Telefone */
+function mouseEmCimaTelefone(imagem) {
+    imagem.src='assets/img/telefone1.png'
+}
+    
+ function mouseForaTelefone(imagem) {
+    imagem.src='assets/img/telefone.png'
+}
+
+
+/* Imgs contato / Email */
+function mouseEmCimaEmail(imagem) {
+    imagem.src='assets/img/gmail.png'
+}
+    
+ function mouseForaEmail(imagem) {
+    imagem.src='assets/img/email.png'
+}
+
+
+/* Imgs contato / Linkedin */
+function mouseEmCimaLinkedin(imagem) {
+    imagem.src='assets/img/linkedin1.png'
+}
+    
+ function mouseForaLinkedin(imagem) {
+    imagem.src='assets/img/linkedin.png'
+}
+
+
+/* Imgs contato / Instagram */
+function mouseEmCimaInstagram(imagem) {
+    imagem.src='assets/img/instagram1.png'
+}
+    
+ function mouseForaInstagram(imagem) {
+    imagem.src='assets/img/instagram.png'
+}
+
